c93b5e6a75ed17eb9a90d4c50ef4b4717558016e
Rui Gonçalves <ruippeixotog@gmail.com>
2011-11-14 16:43:07 +0000

    General UI structure of the app done. Implemented mechanism for registering permission actions.

--- src/pt/up/fe/ssin/pexplorer/app/PermissionInfoActivity.java
+++ src/pt/up/fe/ssin/pexplorer/app/PermissionInfoActivity.java
@@ -1,13 +1,20 @@
 package pt.up.fe.ssin.pexplorer.app;
 
+import pt.up.fe.ssin.pexplorer.R;
 import android.app.Activity;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PermissionInfo;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class PermissionInfoActivity extends Activity {
+
+	private static final String TAB_SPEC_INFO = "info";
+	private static final String TAB_SPEC_ACTIONS = "actions";
+	private static final String TAB_SPEC_APPS = "apps";
 
 	PermissionInfo perm;
 	String extendedInfo = "<More detailed description>";
@@ -26,10 +33,49 @@ public class PermissionInfoActivity extends Activity {
 			return;
 		}
 
-		setTitle(name);
-		TextView tv = new TextView(this);
+		drawActivity();
+	}
+
+	private void drawActivity() {
+		setContentView(R.layout.info_main);
+		setTitle(perm.name);
+		drawTabs();
+
+		drawDetailedInfo();
+		drawActionsList();
+		drawAppsList();
+	}
+
+	private void drawTabs() {
+		TabHost tabHost = (TabHost) findViewById(R.id.tabhost);
+		tabHost.setup();
+
+		TabSpec spec1 = tabHost.newTabSpec(TAB_SPEC_INFO);
+		spec1.setContent(R.id.detailed_info);
+		spec1.setIndicator(getString(R.string.tab_detailed_info));
+		tabHost.addTab(spec1);
+
+		TabSpec spec2 = tabHost.newTabSpec(TAB_SPEC_ACTIONS);
+		spec2.setContent(R.id.actions_list);
+		spec2.setIndicator(getString(R.string.tab_actions_list));
+		tabHost.addTab(spec2);
+
+		TabSpec spec3 = tabHost.newTabSpec(TAB_SPEC_APPS);
+		spec3.setContent(R.id.apps_list);
+		spec3.setIndicator(getString(R.string.tab_apps_list));
+		tabHost.addTab(spec3);
+	}
+
+	private void drawDetailedInfo() {
 		if (perm.descriptionRes != 0)
-			tv.setText(perm.descriptionRes);
-		setContentView(tv);
+			((TextView) findViewById(R.id.test)).setText(perm.descriptionRes);
+	}
+
+	private void drawActionsList() {
+
+	}
+
+	private void drawAppsList() {
+
 	}
 }
