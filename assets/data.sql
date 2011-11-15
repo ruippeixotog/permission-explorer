a207580456fe25c734a9c9824517c3107ac8921a
Daniel Cibrão <daniel.cibrao@gmail.com>
2011-11-15 22:07:34 +0000

    db operational

--- assets/data.sql
+++ assets/data.sql
@@ -1,1 +1,3 @@
-INSERT INTO permissions (name,common,description) VALUES ('android.permission.READ_SMS',1,'Poopy')
+INSERT INTO permissions (name,common,description) VALUES ('android.permission.READ_SMS',1,'Poopy');
+INSERT INTO permissions (name,common,description) VALUES ('android.permission.READ_CONTACTS',1,'LOL')
+
