e893854133cca5c58bce1294f55d968bb11b3a1a
Daniel Cibrão <daniel.cibrao@gmail.com>
2011-11-16 02:45:09 +0000

    bug fixing

--- src/pt/up/fe/ssin/pexplorer/data/PermissionDBOperations.java
+++ src/pt/up/fe/ssin/pexplorer/data/PermissionDBOperations.java
@@ -10,19 +10,7 @@ import android.net.Uri;
 import android.util.Log;
 
 public class PermissionDBOperations{ 	
-	
-	public static boolean createPermission(Context context){
 		
-		ContentValues values = new ContentValues();
-		
-		values.put(PermissionExplorerContentProvider.PERMISSION_NAME, "lulz");
-		values.put(PermissionExplorerContentProvider.PERMISSION_COMMON, 0);
-		values.put(PermissionExplorerContentProvider.PERMISSION_DESCRIPTION,"description lulz");
-		Uri uri = context.getContentResolver().insert(PermissionExplorerContentProvider.CONTENT_URI, values);
-		Log.w("TROLL", "created permission");
-		return (uri != null);
-	}
-	
 	public static String getPermissionDescription(Context context, String name){
         Cursor cursor = context.getContentResolver().query(PermissionExplorerContentProvider.CONTENT_URI, null,  
         		PermissionExplorerContentProvider.PERMISSION_NAME + "= ?", new String[]{name}, null); 
@@ -39,7 +27,7 @@ public class PermissionDBOperations{
         Cursor cursor = context.getContentResolver().query(PermissionExplorerContentProvider.CONTENT_URI, null,  
         		PermissionExplorerContentProvider.PERMISSION_COMMON + "= ?", new String[]{"1"}, null); 
         Set<String> permissions = new HashSet<String>();
-        while (cursor.moveToNext()) { 
+        while (cursor.moveToNext()){ 
                  permissions.add(cursor.getString(cursor.getColumnIndex(PermissionExplorerContentProvider.PERMISSION_NAME))); 
          } 
         cursor.close();
