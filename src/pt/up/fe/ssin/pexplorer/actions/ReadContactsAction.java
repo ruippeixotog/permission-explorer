1a7450ee978a2e1d39923959f0d0d44cd748e9b3
Rui Gonçalves <ruippeixotog@gmail.com>
2012-05-27 22:59:15 +0000

    Added Apache 2.0 notice to all source code.

--- src/pt/up/fe/ssin/pexplorer/actions/ReadContactsAction.java
+++ src/pt/up/fe/ssin/pexplorer/actions/ReadContactsAction.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 Rui Gonçalves and Daniel Cibrão
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package pt.up.fe.ssin.pexplorer.actions;
 
 import java.util.Random;
@@ -42,30 +58,33 @@ public class ReadContactsAction extends PermissionAction {
 		Cursor cur = context.getContentResolver().query(
 				ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 		int random = new Random().nextInt((cur.getCount() - 1) + 1);
-		String name = null, phoneNumber = context.getString(R.string.phone_number_not_available);
+		String name = null, phoneNumber = context
+				.getString(R.string.phone_number_not_available);
 		if (cur.getCount() > 0) {
 			while (cur.moveToNext()) {
-				if (cur.getPosition() == random){
+				if (cur.getPosition() == random) {
 					String id = cur.getString(cur
 							.getColumnIndex(ContactsContract.Contacts._ID));
 					name = cur
 							.getString(cur
 									.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-						if (Integer
-								.parseInt(cur.getString(cur
-										.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-							Cursor pCur = context.getContentResolver().query(
-									ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-									null,
-									ContactsContract.CommonDataKinds.Phone.CONTACT_ID
-											+ " = ?", new String[] { id }, null);
-							while (pCur.moveToNext()) {
-								phoneNumber = pCur
-										.getString(pCur
-												.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-							}
-							pCur.close();
+					if (Integer
+							.parseInt(cur.getString(cur
+									.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+						Cursor pCur = context
+								.getContentResolver()
+								.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+										null,
+										ContactsContract.CommonDataKinds.Phone.CONTACT_ID
+												+ " = ?", new String[] { id },
+										null);
+						while (pCur.moveToNext()) {
+							phoneNumber = pCur
+									.getString(pCur
+											.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 						}
+						pCur.close();
+					}
 					break;
 				}
 			}
